4ac33beee9faa9baa36ca3cf88b738fb36034f3f
juancinfante <juaninfantejj@gmail.com>
2023-07-31 11:28:09 -0300

    Conectando con base de datos

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const app = express();
 require('dotenv').config();
+const {dbConnection} = require('./database/config');
 
 app.listen(process.env.PORT,() => {
     console.log("Servidor corriendo en puerto " + process.env.PORT);
 })
+
+dbConnection();
+
 
 
 
