b000593f73545688e0c1c76ab718f8f56d76b1ca
juancinfante <juaninfantejj@gmail.com>
2023-08-04 01:17:36 -0300

    ruta de eliminar y editar productos  agregada

--- controllers/admin.controller.js
+++ controllers/admin.controller.js
@@ -87,10 +87,62 @@ const obtenerProductos = async (req, res) => {
     }
 }
 
+const eliminarProducto = async (req, res) => {
+	try {
+		const productoEliminar = await Producto.findById(req.params.id);
+
+		if (!productoEliminar) {
+			return res.status(404).json({
+				ok: false,
+				msg: 'No existe un producto con este ID',
+			});
+		}
+
+		await Producto.findByIdAndDelete(req.params.id);
+
+		res.status(200).json({
+			ok: true,
+			msg: 'Producto Eliminado',
+		});
+	} catch (error) {
+		res.status(500).json({
+			ok: false,
+			msg: 'hable con el administrador',
+		});
+	}
+};
+
+const editarProducto = async (req, res) => {
+	try {
+		const productoEditar = await Producto.findById(req.body._id);
+
+		if (!productoEditar) {
+			return res.status(404).json({
+				ok: false,
+				msg: 'No existe ningun Producto con este Id',
+			});
+		}
+
+		await Producto.findByIdAndUpdate(req.body._id, req.body);
+
+		res.status(200).json({
+			ok: true,
+			msg: 'producto editado',
+		});
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({
+			ok: false,
+			msg: 'hable con el administrador',
+		});
+	}
+};
 module.exports = {
     obtenerUsuarios,
     editarUsuario,
     eliminarUsuario,
     agregarProducto,
-    obtenerProductos
+    obtenerProductos,
+    eliminarProducto,
+    editarProducto
 };
--- rutas/admin.js
+++ rutas/admin.js
@@ -1,17 +1,24 @@
 const express = require('express');
 const { check } = require('express-validator');
 const {validarJWT} = require('../middlewares/validar-jwt');
-const { obtenerUsuarios, editarUsuario, eliminarUsuario, agregarProducto, obtenerProductos } = require('../controllers/admin.controller');
+const { obtenerUsuarios, editarUsuario, eliminarUsuario, agregarProducto, obtenerProductos, eliminarProducto, editarProducto } = require('../controllers/admin.controller');
 const router  = express.Router();
 
+// USUARIOS
 router.get('/usuarios', obtenerUsuarios);
 
 router.put('/editarUsuario', editarUsuario);
 
 router.delete('/usuario/:id', eliminarUsuario);
 
+// PRODUCTOS
 router.post('/producto', agregarProducto);
 
 router.get('/productos', obtenerProductos);
+
+router.delete('/producto/:id', eliminarProducto);
+
+router.put('/producto', editarProducto);
+
 
 module.exports = router;
