6faa647ab02682fffecc00c076c7b3e23b6a42d3
juancinfante <juaninfantejj@gmail.com>
2023-08-15 21:48:54 -0300

    heroku file add

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ require('dotenv').config();
 const {dbConnection} = require('./database/config');
 const cors = require('cors');
 
-app.listen(process.env.PORT,() => {
-    console.log("Servidor corriendo en puerto " + process.env.PORT);
+const PORT = process.env.PORT || 4003
+app.listen(PORT,() => {
+    console.log("Servidor corriendo en puerto " + PORT);
 })
 app.use(cors());
 
